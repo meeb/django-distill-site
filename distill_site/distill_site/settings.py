f2f8228dc1c6a238453cce31d7e4eb6f72017bc4
meeb <meeb@meeb.org>
2022-04-30 23:16:47 +1000

    add django-cachekiller

--- distill_site/distill_site/settings.py
+++ distill_site/distill_site/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_distill',
+    'django_cachekiller',
     'sass_processor',
     'common',
     'content',
